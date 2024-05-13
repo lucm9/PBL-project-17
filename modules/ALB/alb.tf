6e7795d6b81fd855bc2b92d2d851bd1f301801d2
luc mally <lucmally@yahoo.com>
2024-05-13 18:00:44 -0500

    Committing local changes to alb.tf

--- modules/ALB/alb.tf
+++ modules/ALB/alb.tf
@@ -3,7 +3,10 @@ resource "aws_lb" "ext-alb" {
   name               = "ext-alb"
   internal           = false
   security_groups    = [aws_security_group.ext-alb-sg.id]
-  subnets            = [aws_subnet.public[0].id, aws_subnet.public[1].id]
+
+  subnets            = [aws_subnet.public[0].id, 
+  aws_subnet.public[1].id]
+  
   ip_address_type    = "ipv4"
   load_balancer_type = "application"
 
